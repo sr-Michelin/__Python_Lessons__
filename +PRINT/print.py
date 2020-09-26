31ab8b5ec5a206db1d7c263a90a01bfa0ffeccc4
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2020-09-26 23:55:54 +0300

    26/09/20

--- +PRINT/print.py
+++ +PRINT/print.py
@@ -2,8 +2,15 @@ n = 'Mike'
 sn = 'Shevchenko'
 
 print("І'мя %s, призвіще %s"%(n,sn))
-print("І'мя {0}, призвіще {1}".format(n,sn))
+print("І'мя {0}, призвіще {1}\n".format(n,sn))
 
-print("\tsfsf\n")
-print('en',end='')
-print("\tsfsf\n")
+t = 'text'
+print('\a{0} -- '.format(t),'\\+a') # дзвінок
+print('\b{0} -- '.format(t),'\\+b') # забій
+print('\n{0} -- '.format(t),'\\+n') # переніс
+print('\f{0} -- '.format(t),'\\+f') # переніс сторінки
+print('\t{0} -- '.format(t),'\\+t') # горизонтальна табуляція
+print('\v{0} -- '.format(t),'\\+v') # вертикальна табуляція
+print('\0 -- '.format(t),'\\+0') # Null
+
+print(r'C:\newt.txt') # відклчення екранування
