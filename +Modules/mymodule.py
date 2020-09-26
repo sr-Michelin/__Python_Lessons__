f2739a3c755bce87b36a978e5f90ed0687fa1833
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2020-09-26 23:58:00 +0300

    26/09/20

--- +Modules/mymodule.py
+++ +Modules/mymodule.py
@@ -2,4 +2,3 @@ def say_hello():
     print('module вітає Ваc!')
 
 __version__ = '0.101'
-
--- +Str/format.py
+++ +Str/format.py
@@ -3,4 +3,3 @@ name = 'Mike'
 
 print('Я {0}, і мені {1} роки'.format(name,age))
 print('{0} любить Python. {0} хоче стати програмістом!'.format(name))
-
