86c0860a6fcd22fd833faa7fa317a7bdc7a2b9e7
sr-Michelin <71037546+sr-Michelin@users.noreply.github.com>
2020-10-18 15:21:46 +0300

    18/10/20

--- +MODULES+/+Pandas/Inexing.py
+++ +MODULES+/+Pandas/Inexing.py
@@ -5,5 +5,15 @@ init()
 
 Frame = pd.read_csv('CSV1.csv', header=0, sep=';')
 print(Fore.GREEN, Frame, '\n')
+
 Frame = Frame.append({'Name': 'Max', 'Sname': 'Varynyca'}, ignore_index=True)
+print(Frame, '\n')
+
+Frame['Birth'] = ['04.11.1998'] + ['01.01.1998'] * 3
+print(Frame, '\n')
+
+'---------------new-------------------'
+print(Frame.dtypes, '\n')  # Тип обєк
+
+Frame.Birth = Frame.Birth.apply(pd.to_datetime)  #
 print(Frame)
