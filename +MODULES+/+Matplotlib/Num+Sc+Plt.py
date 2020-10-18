b46244ebf790e9e47c442f6c4ad55c1bf29caa40
sr-Michelin <71037546+sr-Michelin@users.noreply.github.com>
2020-10-19 02:07:22 +0300

    19/10/20

--- +MODULES+/+Matplotlib/Num+Sc+Plt.py
+++ +MODULES+/+Matplotlib/Num+Sc+Plt.py
@@ -5,7 +5,7 @@ from colorama import init, Fore
 
 init()
 
-x = np.arange(0, 10, 0.1)
+x = np.arange(-10, 10, 0.1)
 y = np.exp(-x / 3.0) + np.random.randn(
     len(x)) * 0.08  # доданок {np.random.randn(len(x)) * 0.08} вказує на наявність шумів у ф-ції
 
@@ -15,7 +15,7 @@ print('Запис трьох перших значень Y: \n', y[:3])
 
 # f = interpolate.interp1d(x,y,kind='linear')
 f = interpolate.interp1d(x, y, kind='quadratic')
-x_new = np.arange(0, 9, 0.1)
+x_new = np.arange(0,9,1)
 y_new = f(x_new)
 
 plt.plot(x, y, 'o', x_new, y_new, '-')
--- +MODULES+/+Matplotlib/pyplot.py
+++ +MODULES+/+Matplotlib/pyplot.py
@@ -1,5 +1,4 @@
 import numpy as np
-from math import log
 import matplotlib.pyplot as plt
 
 '''plt.plot([1,2,3,5],[1,3,4,5])
@@ -9,4 +8,3 @@ x = np.arange(-10,10,0.1)
 y = x**4
 plt.plot(x,y)
 plt.show()
-
