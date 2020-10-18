441375d1491f9981a4841ec8117125e22bf125c3
sr-Michelin <71037546+sr-Michelin@users.noreply.github.com>
2020-10-18 17:32:42 +0300

    18/10/20

--- +MODULES+/+Pandas/Inexing.py
+++ +MODULES+/+Pandas/Inexing.py
@@ -10,11 +10,22 @@ Frame = Frame.append({'Name': 'Max', 'Sname': 'Varynyca'}, ignore_index=True)  #
 print(Frame, '\n')
 
 Frame['Birth'] = ['04.11.1998'] + ['01.01.1998'] * 3  # Новий стовпець
+Frame['Sex'] = [None]*4
 print(Frame, '\n')
 
 '---------------new------------------'
-print(Frame.dtypes, '\n')  # Тип обєкта
 
 Frame.Birth = Frame.Birth.apply(pd.to_datetime)  # Зміна типу подачі дати
-print(Frame)
+print(Frame, '\n')
+
+print(Frame.dtypes, '\n')  # Тип обєкта
+
+print(Frame.info(), '\n')  # Повна інформація
+
+Frame.fillna('Man',inplace=True)  # Заповнення пустих значень {None} новими {'Man'}
+print(Frame, '\n')
+
+print(Frame.Sex,'\n')  # Вивід стовця як масива
+
+print(Frame[['Sex']],'\n')  # Вивід стовця як таблиці
 
