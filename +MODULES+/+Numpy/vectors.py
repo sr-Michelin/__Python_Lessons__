0fab339d857cc5640187e50210d1006ca62356be
sr-Michelin <71037546+sr-Michelin@users.noreply.github.com>
2020-11-20 12:05:07 +0200

    20/11/20

--- +MODULES+/+Numpy/vectors.py
+++ +MODULES+/+Numpy/vectors.py
@@ -5,10 +5,11 @@ from scipy.spatial.distance import cdist
 a = np.array([1, 0, 1], dtype=int)
 b = np.array([True, False, False], dtype=int)
 c = np.arange(0, 10, 1, dtype=int)
-
+L = np.linspace(0, 1, 5, endpoint=1)  # 5 точок у відрізку [0,1]
 print(a, type(a))
 print(b, type(b))
 print(c, type(c))
+print(L, type(L))
 
 print('Розмірність - {0}, вимірність - {1}'.format(a.shape, a.ndim))
 
@@ -48,3 +49,18 @@ print('Скалярний добуток a i b = {0} -- [a.dot(b)]'.format(a.dot
 cos_angle = np.dot(a, b) / (norm(a) * norm(b))
 print('Косинус кута між a i b = {0}'.format(cos_angle))
 print('Кут між a i b = {0}'.format(np.arccos(cos_angle)))
+
+# Витяг масиву в одну строку // наслідування
+_a = a.flatten()  # повертає копію - непряме наслідування
+_a_ = np.array(a)  # копія
+
+_b = a.ravel()  # reshape(-1) - не повертає копію - пряме наслідування
+_b_ = np.asarray(a)  # пряме наслідування
+
+# Агрегуючі ф-ції
+a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print('\nАгрегуючі ф-ції:\nmin:{0},max:{1}, argmax:{2}, sum: {3}, prod:{4}, mean:{5}'
+      .format(a.min(), a.max(), a.argmax(), a.sum(), a.prod(), a.mean()))
+
+a = a.reshape(5, 2)  # двовимірний випадок
+print('Двовимірний випадок [min(axis = 0)]:', a.min(axis=0), '- редукція вздовж стопця.')
---
+++ +MODULES+/+Qiskit/Use Aer's qasm_simulator.py
@@ -0,0 +1,19 @@
+import numpy as np
+from qiskit import (QuantumCircuit, execute, Aer)
+from qiskit.visualization import plot_histogram
+
+sim = Aer.get_backend('qasm_simulator')  # Use Aer's qasm_simulator
+circ = QuantumCircuit(2, 2)  # Create a Quantum Circuit acting on the q register
+circ.h(0)  # Add a H gate on qubit 0
+
+circ.cx(0, 1)  # Add a CX (CNOT) gate on control qubit 0 and target qubit 1
+circ.measure([0, 1], [0, 1])  # Map the quantum measurement to the classical bits
+
+job = execute(circ, sim, shots=1000)  # Execute the circuit on the qasm simulator
+
+res = job.result()  # Grab results from the job
+
+counts = res.get_counts(circ)
+print('\nTotal count for 00 and 11 are:', counts)  # Returns counts
+
+print(circ.draw())  # Draw the circuit
