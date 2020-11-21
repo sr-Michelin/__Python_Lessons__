a356cd2ad5230261f1891bc24f692a12f2880ad0
sr-Michelin <71037546+sr-Michelin@users.noreply.github.com>
2020-11-21 15:43:16 +0200

    21/11/20

---
+++ ++Cousera_python/W2/Task_2.py
@@ -0,0 +1,107 @@
+import numpy as np
+from scipy import linalg
+from math import sin, exp, cos
+import matplotlib.pylab as plt
+
+
+def func(x):
+    return sin(x / 5.) * exp(x / 10.) + 5 * exp(-x / 2.)
+
+
+arr_cord = np.arange(1., 15., 0.1)
+arr_func = np.array([func(coord) for coord in arr_cord])
+
+# СЛАУ першої степені
+print('\n# СЛАУ першої степені')
+arr_cord1 = np.array([1, 15])
+N = 2
+arrA1 = np.empty((0, N))
+for i in range(N):
+    arrA1line = list()
+    for j in range(N):
+        arrA1line.append(arr_cord1[i] ** j)
+    arrA1 = np.append(arrA1, np.array([arrA1line]), axis=0)
+
+arrB1 = np.array([func(coord) for coord in arr_cord1])
+print(arr_cord1)
+print(arrA1)
+print(arrB1)
+
+arrX1 = linalg.solve(arrA1, arrB1)
+print(arrX1)
+
+
+def func1(x):
+    return arrX1[0] + arrX1[1] * x
+
+
+arr_func1 = np.array([func1(coord) for coord in arr_cord])
+
+plt.plot(arr_cord, arr_func, arr_cord, arr_func1)
+plt.show()
+
+# СЛАУ другої степені в т.  1,8,15
+print('\n# СЛАУ другої степені в т.  1,8,15')
+arr_cord2 = np.array([1, 8, 15])
+N = 3
+arrA2 = np.empty((0, N))
+
+for i in range(N):
+    arrA2line = list()
+    for j in range(N):
+        arrA2line.append(arr_cord2[i] ** j)
+    arrA2 = np.append(arrA2, np.array([arrA2line]), axis=0)
+
+arrB2 = np.array([func(coord) for coord in arr_cord2])
+
+print(arr_cord2)
+print(arrA2)
+print(arrB2)
+
+arrX2 = linalg.solve(arrA2, arrB2)
+print(arrX2)
+
+
+def func2(x):
+    return arrX2[0] + arrX2[1] * x + arrX2[2] * (x ** 2)
+
+
+arr_func2 = np.array([func2(coord) for coord in arr_cord])
+plt.plot(arr_cord, arr_func, arr_cord, arr_func1, arr_cord, arr_func2)
+plt.show()
+
+# СЛАУ третьої степені в т. 1,4,10,15
+print('\n# СЛАУ третьої степені в т. 1,4,10,15')
+
+arr_cord3 = np.array([1, 4, 10, 15])
+N = 4
+arrA3 = np.empty((0, N))
+for i in range(N):
+    arrA3line = list()
+    for j in range(N):
+        arrA3line.append(arr_cord3[i] ** j)
+    arrA3 = np.append(arrA3, np.array([arrA3line]), axis=0)
+
+arrB3 = np.array([func(coord) for coord in arr_cord3])
+
+print(arr_cord3)
+print(arrA3)
+print(arrB3)
+
+arrX3 = linalg.solve(arrA3, arrB3)
+print(arrX3)
+
+
+def func3(x):
+    return arrX3[0] + arrX3[1] * x + arrX3[2] * (x ** 2) + arrX3[3] * (x ** 3)
+
+
+arr_func3 = np.array([func3(coord) for coord in arr_cord])
+
+plt.plot(arr_cord, arr_func, arr_cord, arr_func1, arr_cord, arr_func2, arr_cord, arr_func3)
+plt.show()
+
+# Запис у файл
+with open('Task_2.txt', 'w') as f_answer:
+    for i in arrX3:
+        f_answer.write(str(i) + ' ')
---
+++ ++Cousera_python/W3/Task.py
@@ -0,0 +1,80 @@
+import numpy as np
+from math import sin, exp
+import matplotlib.pylab as plt
+from scipy.optimize import minimize, differential_evolution
+
+# Task_1
+print('\nTask_1')
+
+
+def func1(x):
+    return sin(x / 5.) * exp(x / 10.) + 5. * exp(-x / 2.)
+
+
+x_Arr1 = np.arange(1., 31.)
+y_Arr1 = np.array([func1(x) for x in x_Arr1])
+
+min_Fun1_val1 = minimize(func1, np.array(1))
+print('При х = {0}, f(x) = {1} за {2} ітерацій.'.format(round(float(min_Fun1_val1.x), 3), round(min_Fun1_val1.fun, 3),
+                                                        min_Fun1_val1.nit))
+min_Fun1_val2 = minimize(func1, np.array(2), method='BFGS')
+print('При х = {0}, f(x) = {1} за {2} ітерацій -- BFGS [x=2].'.format(round(float(min_Fun1_val2.x), 3),
+                                                                      round(min_Fun1_val2.fun, 3),
+                                                                      min_Fun1_val2.nit))
+min_val_answer1 = np.zeros(2)
+min_val_answer1[0] = round(min_Fun1_val2.fun, 2)
+
+min_Fun1_val3 = minimize(func1, np.array(30), method='BFGS')
+print('При х = {0}, f(x) = {1} за {2} ітерацій -- BFGS [x=30].'.format(round(float(min_Fun1_val3.x), 3),
+                                                                       round(min_Fun1_val3.fun, 3),
+                                                                       min_Fun1_val3.nit))
+min_val_answer1[1] = round(min_Fun1_val3.fun, 2)
+print(min_val_answer1)
+
+with open('Task_1.txt', 'w') as val_answer1:
+    for item in min_val_answer1:
+        val_answer1.write(str(item) + ' ')
+
+# Task_2
+print('\nTask_2')
+bounds = [(1, 30)]
+min_Fun1_val4 = differential_evolution(func1, bounds)
+print('При х = {0}, f(x) = {1} за {2} ітерацій -- Evolution [(1, 30)].'.format(round(float(min_Fun1_val4.x), 3),
+                                                                               round(min_Fun1_val4.fun, 3),
+                                                                               min_Fun1_val4.nit))
+with open('Task_2.txt', 'w') as val_answer2:
+    val_answer2.write(str(round(min_Fun1_val4.fun, 2)))
+
+# Task_3
+print('\nTask_3')
+
+
+def func2(x):
+    return int(func1(x))
+
+
+x_Arr2 = np.arange(1., 31., 0.01)
+y_Arr2 = np.array([func2(x) for x in x_Arr2])
+
+min_Fun2_val1 = minimize(func2, np.array(30), method='BFGS')
+print('При х = {0}, f(x) = {1} за {2} ітерацій -- BFGS [30].'.format(round(float(min_Fun2_val1.x), 3),
+                                                                     round(min_Fun2_val1.fun, 3),
+                                                                     min_Fun2_val1.nit))
+min_val_answer2 = np.zeros(2)
+min_val_answer2[0] = round(min_Fun2_val1.fun, 2)
+
+min_Fun2_val2 = differential_evolution(func2, bounds)
+print('При х = {0}, f(x) = {1} за {2} ітерацій -- Evolution [(1, 30)].'.format(round(float(min_Fun2_val2.x), 3),
+                                                                               round(min_Fun2_val2.fun, 3),
+                                                                               min_Fun2_val2.nit))
+min_val_answer2[1] = round(min_Fun2_val2.fun, 2)
+print(min_val_answer2)
+
+with open('Task_3.txt', 'w') as val_answer3:
+    for item in min_val_answer2:
+        val_answer3.write(str(item) + ' ')
+
+plt.plot(x_Arr1, y_Arr1, x_Arr2, y_Arr2)
+plt.grid(True)
+plt.axis([0, 30, -15, 5])
+plt.show()
