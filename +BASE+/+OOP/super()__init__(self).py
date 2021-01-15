6f348f0f6bf9abe428825a4b93911a6f50707fb9
sr-Michelin <71037546+sr-Michelin@users.noreply.github.com>
2021-01-15 12:58:54 +0200

    15/01/21

---
+++ +BASE+/+OOP/super()__init__(self).py
@@ -0,0 +1,42 @@
+# Cуть superполягає в тому, щоб не уникати написання батьківського класу.
+# Суть полягає в тому, щоб викликати наступний метод у рядку в порядку роздільної здатності методу (MRO).
+# Це стає важливим при багаторазовому успадкуванні.
+
+# super(self.__class__, self).__init__()  # DON'T DO THIS! EVER.
+
+class main():
+    def __init__(self):
+        print('main.__init__(self) - батьківський клас')
+
+
+class inh_0(main):
+    def __init__(self):
+        # main.__init__(self) - явне наслідування (привіт багам і глічам)
+        super().__init__()  # неявне наслідування (більш 'культурний' метод)
+        print('inh_0.__ linit__(self) - наслідування із main')
+
+
+class inh_1(main):
+    def __init__(self):
+        super().__init__()
+        print('inh_1.__ linit__(self) - наслідування із main')
+
+
+class inh_2(main):
+    def __init__(self):
+        super().__init__()
+        print('inh_2.__ linit__(self) - наслідування із main')
+
+
+class inh_3_all(inh_2, inh_1, inh_0):
+    '''\nDiamond structure (ромб, який починається із головного класу, продовжується дочірними,
+і закінчується узагальненням дочірних)'''
+
+    def __init__(self):
+        super().__init__()
+        print('inh_3.__ linit__(self) - узагальнення дочірних підкласів')
+
+
+J = inh_3_all()
+print(inh_3_all.__doc__)
+print(inh_3_all.mro())
