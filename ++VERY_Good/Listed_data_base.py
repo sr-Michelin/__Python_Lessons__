c4ba22baaf5a0178e540a06fd0407b0eef8e2ded
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2021-02-03 18:26:11 +0200

    03/02/2021

--- ++VERY_Good/Listed_data_base.py
+++ ++VERY_Good/Listed_data_base.py
@@ -9,16 +9,16 @@ class present_js(units):
     def __init__(self, origin, volume, price, quality):
         # units.__init__(self, volume, price, quality)
         super().__init__(volume, price, quality)
-        print('We have {0} juice with volume {1}L, priece {2} UAN ans quality {3}%'.format(origin, volume,
-                                                                                           price, quality))
+        print('We have {0} juice with volume {1}L, price {2} UAN ans quality {3}%'.format(origin, volume,
+                                                                                          price, quality))
 
 
-# Js_ = [present_js('Apple', 10, 19, 59), present_js('Orrange', 5, 25, 91), present_js('Blueberry', 1, 38, 99)]
+# Js_ = [present_js('Apple', 10, 19, 59), present_js('Orange', 5, 25, 91), present_js('Blueberry', 1, 38, 99)]
 Js_ = []
 run = 1
 a = 1
 while run:
-    A = input('\nCraete new juice (y/n)? ')
+    A = input('\nCreate new juice (y/n)? ')
 
     if A == 'y':
         print('Juice {}: '.format(a))
@@ -30,20 +30,20 @@ while run:
         run = 0
 
 
-def averrage():
+def average():
     sum_vol = 0
     sum_price = 0
     sum_quality = 0
 
     if len(Js_) > 0:
-        print('Let\'s go to averrage\'s')
+        print('Let\'s go to average\'s')
         for n in range(0, len(Js_)):
             sum_vol += Js_[n].volume
             sum_price += Js_[n].price
             sum_quality += Js_[n].quality
 
-        print('_' * 80, '\nAverrages of {0} different juices: '
-                        'volume - {1}L, priece - {2} UAN, quality - {3}%'.
+        print('_' * 80, '\nAverages of {0} different juices: '
+                        'volume - {1}L, price - {2} UAN, quality - {3}%'.
               format(len(Js_), round(sum_vol / len(Js_), 2), round(sum_price / len(Js_), 2),
                      round(sum_quality / len(Js_), 2)), '\n{}'.format('_' * 80))
     else:
@@ -51,6 +51,6 @@ def averrage():
 
 
 if __name__ == '__main__':
-    averrage()
+    average()
 
 input()
