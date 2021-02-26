7e2c01d43cf42ecfde039995501560fbe7bc48c3
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2021-02-26 22:33:18 +0200

    26/02/21

---
+++ +BASE+/+Functions/Map().py
@@ -0,0 +1,15 @@
+def sq(x):
+    return x ** 2
+
+
+lst = [i for i in range(11)]
+M = map(sq, lst)
+
+for i in lst:
+    print(next(M), end=' ')
+
+# ----------------------------------------
+print('\r')
+lst_1 = ['Львів', 'Київ', 'Харків', 'Одеса', 'Кривий ріг', 'Луганськ']
+b = list(map(len, lst_1))
+print(b)
