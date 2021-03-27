369794b52f67f7bbd33f0f7709f0efed7f741f2e
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2021-03-27 02:13:30 +0200

    27.03.21

---
+++ +MODULES+/+Django/django.py
@@ -0,0 +1,13 @@
+import os
+'''cwd = os.getcwd()
+print(cwd)'''
+
+# тестовий запуск:
+'''first_dj_site>python manage.py runserver'''
+'''first_dj_site>python manage.py runserver 4000'''
+'''first_dj_site>python manage.py runserver 1.2.3.4:4000'''
+
+# добавлення веб-застосунку(аплікації) news - папки із пакетами:
+'''first_dj_site>python manage.py startapp news'''
+
+# controller - news.views  --- між даними і їх відображеннями
