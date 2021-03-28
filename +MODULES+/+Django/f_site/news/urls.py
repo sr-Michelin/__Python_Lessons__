2532ac5b95d198b8f4e62f8b27db6903485f3e48
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2021-03-28 22:12:26 +0300

    28/03/21

--- +MODULES+/+Django/f_site/news/urls.py
+++ +MODULES+/+Django/f_site/news/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import index, test
+from .views import index
 
 # збереження списку маршрутів зберігаємо у апці, а не в скелеті самого сайту (так забезпечується незалежність апок)
 # відкидання лишніх адрес (/news/test)
 urlpatterns = {
     path('', index),
-    path('test/', test),
+    # path('test/', test),
 }
--- +MODULES+/+Django/f_site/news/views.py
+++ +MODULES+/+Django/f_site/news/views.py
@@ -8,14 +8,15 @@ from .models import News
 def index(request):
     """Вивід новин і початкового тексту"""
     news = News.objects.all()
-    res = '<h1>Список новин</h1>'
+    '''res = '<h1>Список новин</h1>'
     for item in news:
         res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n<div>\n<hr>\n'
 
-    return HttpResponse(['<h1>It is my first site!</h1>', res])
+    # return HttpResponse(['<h1>It is my first site!</h1>', res])
     # print(request)
-    # return HttpResponse(res)
+    # return HttpResponse(res)'''
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новин'})
 
 
-def test(request):
-    return HttpResponse('<h1>Testing page...</h1>')
+#def test(request):
+#   return HttpResponse('<h1>Testing page...</h1>')'''
