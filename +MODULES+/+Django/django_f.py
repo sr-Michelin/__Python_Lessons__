310b55d440c301e8a5ab2b26b01c3fc6f0de52f8
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2021-03-28 20:06:19 +0300

    28/03/21

--- +MODULES+/+Django/django_f.py
+++ +MODULES+/+Django/django_f.py
@@ -28,34 +28,57 @@ print(cwd)
 
 # python manage.py shell -- інтерактивна консоль Django:
 
-'''////////////////////////////////'''
+'''////////////////CRUD////////////////'''
 # from news.models import News
 
+'''запис нового запису'''
 # News(title='Новина №1', content='Контент новини №1')
 # news = News  # news = _
 # news.id
 # news.save() # збереження обєкта публікації
 # news.id
 
+'''SQL запит у вигляді словника'''
 # from django.db import connection
 # connection.queries # SQL запит у вигляді словника
 # news.pk # news.id - первинний ключ запису, який доступний після збереження запису
 
+'''запис нового запису'''
 # news2 = News(title='Новина №2', content='Контент новини №2') - запис другої новини
 # news2.save()
 
+'''ще один запис нового запису'''
 # news3 = News()
 # news3.title = 'Новина №3'
 # news3.content = 'Контент новини №3'
 # news3.save()
 
+'''без збереження через save()'''
 # news4 = News.objects.create(title='Новина №4', content='Контент новини №4')  # вже без збереження через save()
 
-
+'''перегляду усіх новин'''
 # News.objects.all() - для перегляду усіх новин
-
 # news = _  # ітератор
 # for item in news:
 #    print(item.title, item.is_published)
 
-# News.objects.filter(title = 'Новина №1') # фільтр
+'''фільтр'''
+# News.objects.filter(title = 'Новина №1')
+# News.objects.get(pk = 1) - для одного значення - по pk
+
+'''присвоєння нових значеннь старим записам:'''
+# News.objects.get(pk = 4)
+# news3 = _
+# news3.pk = 3
+# news3.save()
+
+'''видалення запису:'''
+# news9 = News.objects.get(pk = 9)
+# news9.delete()
+
+'''виведення за певним порядком - сортування'''
+# News.objects.order_by('title')
+# News.objects.order_by(-'title') # зворотній порядок
+
+'''викючення певного запису у виводі (не у базі:)'''
+# News.objects.exclude(title = 'Новина №4')
