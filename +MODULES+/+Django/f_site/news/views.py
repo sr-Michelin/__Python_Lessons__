2701e1affe008c1073102afdf8a438ce7c6f27f6
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2021-03-28 20:34:25 +0300

    28/03/21

--- +MODULES+/+Django/f_site/news/views.py
+++ +MODULES+/+Django/f_site/news/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 # Create your views here.
 
 def index(request):
-    print(request)
-    return HttpResponse('<h1>It is my first site!</h1>')
+    """Вивід новин і початкового тексту"""
+    news = News.objects.all()
+    res = '<h1>Список новин</h1>'
+    for item in news:
+        res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n<div>\n<hr>\n'
+
+    return HttpResponse(['<h1>It is my first site!</h1>', res])
+    # print(request)
+    # return HttpResponse(res)
 
 
 def test(request):
