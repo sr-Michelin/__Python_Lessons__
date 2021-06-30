2b16c26344826460c7221fa7c7bddd1840e7d696
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2021-06-30 22:18:39 +0300

    30/06/21

---
+++ +BASE+/+Functions/Global.py
@@ -0,0 +1,27 @@
+A = 0
+
+
+def fun(x):
+    """Використання глобальної змінної А (метод global)"""
+    global A
+    A = 1
+    print(f'{A**x = }')
+
+
+def fun_out():
+    """Зовнішня ф-ція"""
+    A = 1
+
+    def fun_in():
+        """Внутрішня ф-ція"""
+        nonlocal A  # зміна А=1 зовнішньої ф-ції на А=2 із внутрішньої (крок назовні)
+        A = 2
+        print(f'In {A = }')
+
+    fun_in()
+    print(f'Out {A = }')
+
+
+print(f'Global {A = }')
+fun(1)
+fun_out()
